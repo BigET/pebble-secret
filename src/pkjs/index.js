50c7c797ee5dbfec0a983800bf52d45157a6374e
Eduard Budulea <ebudulea@oss.com>
2017-02-05 23:29:48 +0200

    Adding configuration companion.

---
+++ src/pkjs/index.js
@@ -0,0 +1,22 @@
+Pebble.addEventListener('ready', function() {console.log('PebbleKit JS ready!');});
+
+Pebble.addEventListener('showConfiguration', function() {
+    var url = 'https://rawgit.com/pebble-examples/design-guides-slate-config/master/config/index.html';
+    console.log('Showing configuration page: ' + url);
+
+    Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+    var configData = JSON.parse(decodeURIComponent(e.response));
+    console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+    var dict = {
+        "SecretTitle":configData["SecretTitle"],
+        "SecretText":configData["SecretText"],
+        "EncriptionKey":configData["EncriptionKey"]
+    };
+    Pebble.sendAppMessage(dict,
+        function() {console.log('Send successful: ' + JSON.stringify(dict));},
+        function() {console.log('Send failed!');});
+});
